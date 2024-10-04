a69319b6300f3ebef3a2da75327f3eb677677b9f
JONATHAN <jfernandez30@uan.edu.co>
2024-10-04 13:20:59 -0500

    Se implemento la opcion de recuperar contraseña

--- src/application/sendEmail.py
+++ src/application/sendEmail.py
@@ -3,6 +3,18 @@ from dotenv import load_dotenv
 from email.message import EmailMessage
 import ssl
 import smtplib
+from flask import request, url_for
+import secrets
+from itsdangerous import URLSafeTimedSerializer
+
+# Crear un token único con vencimiento
+
+secret_key = secrets.token_hex(16)
+
+def generar_token(email):
+    serializer = URLSafeTimedSerializer('SECRET_KEY')
+    token = serializer.dumps(email, salt='password-reset-salt')
+    return token
 
 # Se cargan las variables de entorno
 load_dotenv()
@@ -10,12 +22,10 @@ load_dotenv()
 email_sender = 'tsumekireisf2@gmail.com'
 password = os.getenv('PASSWORD_EMAIL') # Se trae del archivo .env la variable de entorno que contiene la contraseña
 
-em = EmailMessage() # Inicializamos el objeto que nos permite usar las funciones de correo
-
-
 context = ssl.create_default_context() # Se crea un contexto seguro encriptando el email
 
 def send_email_signup(name, email_reciber):
+    em = EmailMessage() # Inicializamos el objeto que nos permite usar las funciones de correo
     subject = 'Successful Sign up'
     body = f'{name}, tu regsitro en la pagina web de Tsume Kirei ha sido exitoso.'
     em["From"] = email_sender
@@ -29,8 +39,12 @@ def send_email_signup(name, email_reciber):
         smtp.sendmail(email_sender, email_reciber, em.as_string())
 
 def send_recover_password(name, email_reciber):
+    em = EmailMessage() # Inicializamos el objeto que nos permite usar las funciones de correo
+    # Enviar correo con el link para restablecer la contraseña
+    token = generar_token(email_reciber)
+    reset_url = url_for('reset_password', token=token, _external=True)  # Genera la URL del link
     subject = 'Recover password'
-    body = f'{name}, usa el siguiente ling para recuperar contraseña. http://127.0.0.1:5000/recover_password'
+    body = f'{name}, Para restablecer tu contraseña, haz clic en el siguiente enlace: {reset_url}'
     em["From"] = email_sender
     em["To"] = email_reciber
     em['Subject'] = subject
