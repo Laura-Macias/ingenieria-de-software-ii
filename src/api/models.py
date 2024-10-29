a3f020539da92f363efab80f5481c692628e21e9
Sebastian Ceballos <jhonquenan2003@gmail.com>
2024-10-29 16:48:55 -0500

    Se integra el funcionamiento de catalogo

--- src/api/models.py
+++ src/api/models.py
@@ -1,8 +1,36 @@
 from flask_login import UserMixin
+from config import Config
+
 
 # Definir la clase User que hereda de UserMixin para compatibilidad con Flask-Login
 class User(UserMixin):
+
     def __init__(self, id_customer, name, email):
         self.id = id_customer 
         self.name = name
         self.email = email
+
+class Catalogo :
+
+    def __init__(self, id_catalog, product_name, description, price, stock, update_date):
+
+      self.id_catalog = id_catalog
+      self.product_name = product_name
+      self.description = description
+      self.price = price
+      self.stock = stock
+      self.update_date = update_date
+
+    
+    def serialize(self):
+        return {
+            'id_catalog': self.id_catalog,
+            'product_name': self.product_name,
+            'description': self.description,
+            'price': self.price,
+            'stock': self.stock,
+            'update_date': self.update_date
+        }
+
+
+
