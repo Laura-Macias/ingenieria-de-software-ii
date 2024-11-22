2e014677dfd62cc635306a29bcf916c367719b11
Sebastian Ceballos <jhonquenan2003@gmail.com>
2024-11-22 03:34:27 -0500

    Se trae la informacion de la base de datos para usar la tabla detail

--- src/api/detalle.py
+++ src/api/detalle.py
@@ -1,8 +1,33 @@
 from flask import Blueprint, render_template
+from .models.detail import Detail
+from config import Config
 
 # Definir un Blueprint para la vista detalles
-detalle_bp = Blueprint('detalle', __name__)
+detalle_bp = Blueprint('detalle', __name__, url_prefix='/detalle')
+supabase = Config.supabase
 
-@detalle_bp.route('/detalle')
+
+def detalle():
+        
+    try:
+        response = supabase.table('Detail').select('id_detail, id_customer, id_service, id_staff, date, Catalog(id_catalog, service_name, price)').execute()
+        if not response.data:
+            print("No se encontraron detalles en la base de datos.")
+            
+            return []
+        return [Detail(**item) for item in response.data]
+    except Exception as e:
+        print(f"Error al obtener el detalle: {e}")
+        return []
+
+
+#return render_template('carrito.html')
+
+@detalle_bp.route('/')
 def detalle_vacio():
-    return render_template('carrito.html')
+    servicios = detalle()  # Llama a la función que obtiene el detalles
+    if servicios:  # Verifica si hay productos antes de renderizar
+        return render_template('carrito.html', servicios=servicios)  # Pasa los productos al template
+    else:
+        return render_template('carrito.html', servicios=[])  # Renderiza con una lista vacía si no hay productos
+
---
+++ src/api/models/detail.py
@@ -0,0 +1,10 @@
+class Detail : 
+
+    def __init__(self, id_detail, id_customer, id_service, id_staff, time, date ):
+
+        self.id_detail = id_detail
+        self.id_customer = id_customer
+        self.id_service = id_service
+        self.id_staff = id_staff
+        self.time = time
+        self.date = date
