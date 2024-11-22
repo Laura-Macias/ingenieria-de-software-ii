fa439e9aa3ebeef64e0e60a23ff97d7271b0b113
aleherrera23 <aleherreraroa23@gmail.com>
2024-11-21 23:36:38 -0500

    Se realizan los ajustes en el calendario para integrar boton de pago, se ajusta la vista de detalle para que muestre la información y se realizan algunos ajustes en la redirección y estilo de la pasarela de pagos

--- src/api/detalle.py
+++ src/api/detalle.py
@@ -3,10 +3,6 @@ from flask import Blueprint, render_template
 # Definir un Blueprint para la vista detalles
 detalle_bp = Blueprint('detalle', __name__)
 
-@detalle_bp.route('/detalle_vacio')
+@detalle_bp.route('/detalle')
 def detalle_vacio():
-    return render_template('carrito_vacio.html')
-
-@detalle_bp.route('/detalle_lleno')
-def detalle_lleno():
-    return render_template('carrito_lleno.html')
+    return render_template('carrito.html')
